e029bf45dd2472968c90a71b8bdca3751b270992
Daniel Warkus <daniel.warkus@hhu.de>
2024-01-14 22:39:13 +0100

    playing with functions

--- BFR_ex_1.py
+++ BFR_ex_1.py
@@ -1,20 +1,27 @@
 import numpy as np
 from sklearn.cluster import KMeans
 
+
 class BFR:
 
     def __init__(self, k, data):
         self.k = k
         self.d = data
-        self.kmeans = KMeans(n_clusters=k, random_state=0)
-        self.DS = {}
-        self.CS = {}
-        self.RS = {}
+        self.kmeans = KMeans(n_clusters=k, random_state=0, init='k-means++')
+        self.DS = [[] for i in range(k)]
+        self.CS = []
+        self.RS = []
 
-    def fit(self, data):
+        # Sets gehen leider nicht da sie immutable sind
+
+    def select_k_random_points(self, data):
         self.kmeans.fit(data)
-        labels = self.kmeans.labels_
+        centroids = self.kmeans.cluster_centers_
 
-        return labels
+        for i, centroid in enumerate(centroids):
+            self.DS[i].append(centroid)
 
+        #SUM = [np.sum(centroids[i]) for i in range(N)]
+        #SUMQ = [np.sum(centroids[i] ** 2) for i in range(N)]
 
+        return self.DS
