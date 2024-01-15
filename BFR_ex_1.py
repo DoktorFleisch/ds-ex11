3fa29282b50d0afb3c81bf467d306fccd35b5c3f
Daniel Warkus <daniel.warkus@hhu.de>
2024-01-15 23:34:33 +0100

    Erste Funktionen für berechnungen der Distanzen

--- BFR_ex_1.py
+++ BFR_ex_1.py
@@ -12,7 +12,24 @@ class BFR:
         self.CS = []
         self.RS = []
 
-        # Sets gehen leider nicht da sie immutable sind
+    def calculate_variance(cluster):
+        return (cluster['SUMQ'] / cluster['N']) - (cluster['SUM'] / cluster['N']) ** 2
+
+    def calculate_standard_deviation(variance):
+        return np.sqrt(variance)
+
+    def calculate_normalized_distance(self, centroid, point, standard_deviation):
+        normalized_distances = [(p - c) / sd for p, c, sd in zip(point, centroid, standard_deviation)]
+
+        return normalized_distances
+
+    def calculate_Mahalanobis(self, cluster, point):
+        centroid = cluster['SUM'] / cluster['N']
+        variance = self.calculate_variance(cluster)
+        standard_deviation = self.calculate_standard_deviation(variance)
+        normalized_distances = self.calculate_normalized_distance(centroid, point, standard_deviation)
+
+        return np.sqrt(np.sum(np.square(normalized_distances)))
 
     def select_k_random_points(self, data):
         self.kmeans.fit(data)
@@ -29,3 +46,17 @@ class BFR:
         #SUMQ = [np.sum(centroids[i] ** 2) for i in range(N)]
 
         return self.DS
+
+    def fit(self, data):
+       # Schritt 1: Punkte sind nah einem Cluster von DS: Füge zu DS hinzu und update SUM, SUMQ, N für das jeweilige Cluster.
+
+       # Schritt 2: Keiner der Punkte ist nah einem Cluster von DS: Nutze Kmeans zum Clustern dieser übrig geblieben Punkte, und die
+       # aus dem alten RS-Set. Entstehende Cluster gehen in CS, und die outliner in RS.
+
+       # Schritt 3: Ggfs. Merge Cluster in CS.
+
+       # Schritt 4: Merge die Cluster in DS und CS, die nah beieinander liegen. Merge Punkte in RS mit den Clustern in DS.
+
+
+
+        return self.DS, self.CS, self.RS
